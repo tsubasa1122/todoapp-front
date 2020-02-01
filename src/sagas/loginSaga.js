90d68a736d4708e6aabbbcef3ae9c3e2e8f34b22
tsubasa1122 <nakanot@open8.com>
2020-02-02 08:39:58 +0900

    [add]actionが成功したら、root_pathへリダイレクトさせる

--- src/sagas/loginSaga.js
+++ src/sagas/loginSaga.js
@@ -1,13 +1,15 @@
 import { call, put, takeEvery } from 'redux-saga/effects';
-import api from '../common/api';
+import loginApi from '../common/api';
 import { loginSuccess, loginFailure } from '../actions';
+import { routerActions } from 'react-router-redux';
 
 function* handleRequest(action) {
   let { payload } = action;
-  payload = yield call(api, '/api/auth/sign_in', 'POST', payload);
+  payload = yield call(loginApi, '/api/auth/sign_in', 'POST', payload);
 
-  if (payload && !payload.errors) {
+  if (payload) {
     yield put(loginSuccess(payload));
+    yield put(routerActions.push('/'));
   } else {
     yield put(loginFailure(payload));
   }
